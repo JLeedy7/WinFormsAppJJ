a833665d23b974923b6b0b4c7eabfcb04b5cf9ee
Jon41 <Jon41@DESKTOP-D5UUMJS>
2025-02-02 19:35:14 -0800

    Add project files.

---
+++ WinFormsAppJJ/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WinFormsAppJJ
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            txtDueDate = new TextBox();
+            txtTaskDescription = new TextBox();
+            lblTaskBoxLabel = new Label();
+            lblDueDate = new Label();
+            btnClear = new Button();
+            btnAdd = new Button();
+            gbTaskList = new GroupBox();
+            lbTaskList = new ListBox();
+            groupBox1.SuspendLayout();
+            gbTaskList.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(txtDueDate);
+            groupBox1.Controls.Add(txtTaskDescription);
+            groupBox1.Controls.Add(lblTaskBoxLabel);
+            groupBox1.Controls.Add(lblDueDate);
+            groupBox1.Controls.Add(btnClear);
+            groupBox1.Controls.Add(btnAdd);
+            groupBox1.Location = new Point(4, 9);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(297, 158);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Task";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // txtDueDate
+            // 
+            txtDueDate.Location = new Point(69, 48);
+            txtDueDate.Name = "txtDueDate";
+            txtDueDate.PlaceholderText = "2025-02-15";
+            txtDueDate.Size = new Size(228, 23);
+            txtDueDate.TabIndex = 5;
+            txtDueDate.TextChanged += txtDueDate_TextChanged;
+            // 
+            // txtTaskDescription
+            // 
+            txtTaskDescription.Location = new Point(69, 18);
+            txtTaskDescription.Name = "txtTaskDescription";
+            txtTaskDescription.Size = new Size(228, 23);
+            txtTaskDescription.TabIndex = 4;
+            txtTaskDescription.TextChanged += txtTaskDescription_TextChanged;
+            // 
+            // lblTaskBoxLabel
+            // 
+            lblTaskBoxLabel.AutoSize = true;
+            lblTaskBoxLabel.Location = new Point(10, 21);
+            lblTaskBoxLabel.Name = "lblTaskBoxLabel";
+            lblTaskBoxLabel.Size = new Size(29, 15);
+            lblTaskBoxLabel.TabIndex = 3;
+            lblTaskBoxLabel.Text = "Task";
+            // 
+            // lblDueDate
+            // 
+            lblDueDate.AutoSize = true;
+            lblDueDate.Location = new Point(8, 51);
+            lblDueDate.Name = "lblDueDate";
+            lblDueDate.Size = new Size(55, 15);
+            lblDueDate.TabIndex = 2;
+            lblDueDate.Text = "Due Date";
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(141, 120);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(75, 23);
+            btnClear.TabIndex = 1;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(222, 120);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(75, 23);
+            btnAdd.TabIndex = 0;
+            btnAdd.Text = "Add Todo";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += submitForm;
+            // 
+            // gbTaskList
+            // 
+            gbTaskList.Controls.Add(lbTaskList);
+            gbTaskList.Location = new Point(7, 173);
+            gbTaskList.Name = "gbTaskList";
+            gbTaskList.Size = new Size(294, 399);
+            gbTaskList.TabIndex = 1;
+            gbTaskList.TabStop = false;
+            gbTaskList.Text = "Task List";
+            gbTaskList.Enter += gbTaskList_Enter;
+            // 
+            // lbTaskList
+            // 
+            lbTaskList.FormattingEnabled = true;
+            lbTaskList.ItemHeight = 15;
+            lbTaskList.Location = new Point(6, 18);
+            lbTaskList.Name = "lbTaskList";
+            lbTaskList.Size = new Size(288, 379);
+            lbTaskList.TabIndex = 0;
+            lbTaskList.SelectedIndexChanged += lbTaskList_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(303, 575);
+            Controls.Add(gbTaskList);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            gbTaskList.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private TextBox txtDueDate;
+        private TextBox txtTaskDescription;
+        private Label lblTaskBoxLabel;
+        private Label lblDueDate;
+        private Button btnClear;
+        private Button btnAdd;
+        private GroupBox gbTaskList;
+        private ListBox lbTaskList;
+    }
+}
---
+++ WinFormsAppJJ/Form1.cs
@@ -0,0 +1,121 @@
+using WinformTodo;
+
+namespace WinFormsAppJJ
+{
+    public partial class Form1 : Form
+    {
+        private List<Todo> TaskList {  get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            TaskList = new List<Todo>();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearForm();
+            MessageBox.Show("Form Cleared");
+        }
+
+        private void ClearForm()
+        {
+            txtTaskDescription.Clear();
+            txtDueDate.Clear();
+        }
+
+        private void lbTaskList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex = lbTaskList.SelectedIndex;
+            string selectedItem = (string)lbTaskList.SelectedItem;
+
+            if (selectedIndex == -1)
+            {
+                return;
+            }
+
+            if (selectedItem == null)
+            {
+                MessageBox.Show("No item selected at the index.");
+                return;
+            }
+
+            int id = Int32.Parse(selectedItem.Split(" - ")[0]);
+            var todo = TaskList.Find(t=>t.Id == id);
+            if (todo != null)
+            {
+                todo.IsDone = !todo.IsDone;
+                UpdateListBox();
+            }
+        }
+
+        private void gbTaskList_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submitForm(object sender, EventArgs e)
+        {
+            if (Validators.IsEmptyText(txtTaskDescription))
+            {
+                MessageBox.Show("Description is empty, please provide a description");
+                return;
+            }
+            
+            if (Validators.IsTextNull(txtTaskDescription))
+            {
+                MessageBox.Show("Description needs to be created");
+                return;
+            }
+
+            if (Validators.IsEmptyText(txtDueDate))
+            {
+                MessageBox.Show("Missing a due date.");
+                return;
+            }
+
+            if (!Validators.IsValidDate(txtDueDate))
+            {
+                MessageBox.Show("Date is incorrectly formatted, please resubmit.");
+                return;
+            }
+
+            Todo myTodo = new Todo(txtTaskDescription.Text, DateTime.Parse(txtDueDate.Text));
+
+            TaskList.Add(myTodo);
+            UpdateListBox();
+            ClearForm();
+        }
+
+        public void UpdateListBox()
+        {
+            lbTaskList.Items.Clear();
+            var list = TaskList
+            .OrderBy(t =>t.DueDate)
+            .ToList();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                lbTaskList.Items.Add(list[i].ToString());
+            }
+        }
+        private void txtDueDate_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTaskDescription_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
